3c3c3749e4698d140006cc757ea43fc557a7b56b
borjanG <geshkovskiborjan@gmail.com>
2020-02-04 20:01:52 +0100

    at least now the graphs are showing, but idk if they make sense

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from sklearn import datasets
 from sklearn.datasets import make_classification
 import neural_net as nn
 import seaborn as sns
-sns.set(style="darkgrid")
-#sns.set(style="whitegrid")
+#sns.set(style="darkgrid")
+sns.set(style="whitegrid")
 
 # Colormap options: plt.cm.jet, plt.cm.coolwarm, plt.cm.viridis
 
@@ -98,7 +98,7 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
     datasets_ = {'blobs': datasets.make_blobs(n_samples=samples, n_features=features, centers=2, cluster_std=1, random_state=2), 
                  'spirals': datasets.make_moons(n_samples=samples, noise=0.2),
                  'chess': generate_points(samples, [(0, 0), (0, 1), (1, 0), (1, 1)], [0, 1, 1, 0], 0.005),
-                 'q_random': generate_points_1d(nb=samples, centers=[-1, 0, 1, 2], labels=[1, 0, 1, 0])}
+                 'q_random': generate_points_1d(nb=samples, centers=[-1, 0, 1], labels=[1, 0, 1])}
     data = datasets_[data_]
     
     if data_ == "chess" or data_ == "q_random":
@@ -108,10 +108,11 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
         y = np.expand_dims(data[1], 1).T   
         
     network = nn.NeuralNetwork(architecture, seed=0)
-    history = network.train(X=X, y=y, batch_size=3, epochs=5000, learning_rate=0.3, 
+    history = network.train(X=X, y=y, batch_size=16, epochs=5000, learning_rate=0.3, 
                                print_every=1000, validation_split=0.2, tqdm_=False,
                                plot_every=2500)
-    weights, biases = history['weights'], history['biases']
+    
+    #weights, biases = history['weights'], history['biases']
      
     # Initialize the neural network scheme
     # np.shape(z0) = (input_dimension, samples)
@@ -182,15 +183,32 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
             # The reshape has the effect of giving an array of N_{k-1} rows
             # and 1 column (1 column because we do this for all i)
             Lambdak_datai = network.Lambda(k, zkmoins1_list[:, i].reshape(architecture[k-1], 1))
+            
             # We fill the i-th column of every row with this linear transition
             # Things should be consistent because Lambdak has N_k rows, and
             # every element will have N_{k-1} columns
             Lambdak_list[:, i] = Lambdak_datai.reshape(-1)
-            zk_list[:, 1] = nn.sigmoid(Lambdak_datai).reshape(-1)
+            zk_list[:, i] = nn.sigmoid(Lambdak_datai).reshape(-1)
         transitions.append(Lambdak_list)
         layers.append(zk_list)
-
-    # Plotting the linear transitions
+     
+#    print('The data')
+#    print(z0)
+#    print('The first weight and bias')
+#    print('A0=', weights[0])
+#    print('b0=', biases[0])        
+#    print('First linear')
+#    print(transitions[0])
+#    
+##    print('Second linear')
+##    print(transitions[1])
+##    
+#    # Il y a un problème ici..
+#    print('First activation')
+#    print(layers[1])
+##    print('Second activation')
+##    print(layers[2])
+        
     for i, lbd in enumerate(transitions):
         if len(lbd) == 2:
             plt.figure()
@@ -209,6 +227,7 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
         else:
             red = list()
             blue = list()
+            lbd = copy.copy(lbd.reshape(-1))
             for j, e in enumerate(lbd):
                 if y[0][j] == 0:
                     blue.append(e)
@@ -224,21 +243,27 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
         
     for i, sig in enumerate(layers):
         if i==0:
-            # Don't want to show input layer as we already plot it
             pass
         else:
             if len(sig) == 2:
                 plt.figure()
                 plt.scatter(sig.T[:, 0], sig.T[:, 1], c=y.T.reshape(-1), cmap=plt.cm.coolwarm, alpha=0.55)
-                plt.xlabel(r'$z^{}_1$ coordinate'.format(j), fontdict = {'fontsize' : 12})
-                plt.ylabel(r'$z^{}_2$ coordinate'.format(j), fontdict = {'fontsize' : 12})
+                plt.xlabel(r'$z^{}_1$ coordinate'.format(i), fontdict = {'fontsize' : 12})
+                plt.ylabel(r'$z^{}_2$ coordinate'.format(i), fontdict = {'fontsize' : 12})
                 plt.title(r'{}st hidden layer: $z^{} = \sigma(A^{} z^{} + b^{})$'.format(i, i, i-1, i-1, i-1), 
                       fontdict = {'fontsize' : 18})
             elif len(sig) == 3:
-                pass
+                fig = plt.figure()
+                ax = Axes3D(fig)
+                ax.scatter(sig.T[:, 0], sig.T[:, 1], sig.T[:, 2], c=y.T.reshape(-1), cmap=plt.cm.coolwarm, alpha=0.55)
+                plt.xlabel(r'$z^{}_1$ coordinate'.format(i), fontdict = {'fontsize' : 12})
+                plt.ylabel(r'$z^{}_2$ coordinate'.format(i), fontdict = {'fontsize' : 12})
+                plt.title(r'{}st hidden layer: $z^{} = \sigma(A^{} z^{} + b^{})$'.format(i, i, i-1, i-1, i-1), 
+                      fontdict = {'fontsize' : 18})
             else:
                 red = list()
                 blue = list()
+                sig = copy.copy(sig.reshape(-1))
                 for j, e in enumerate(sig):
                     if y[0][j] == 0:
                         blue.append(e)
@@ -249,8 +274,8 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
         
                 plt.plot(blue, len(blue)*[0], 'o', c='b', alpha=0.55)
                 plt.plot(red, len(red)*[0], 'o', c='r', alpha=0.55)
-                plt.xlabel(r'$z^{} \in$ ℝ coordinate'.format(j), fontdict = {'fontsize' : 12})
-                if j == len(layers)-1:
+                plt.xlabel(r'$z^{} \in$ ℝ coordinate'.format(i), fontdict = {'fontsize' : 12})
+                if i == len(layers)-1:
                     plt.title(r'Output of neural network: $z^{} = \sigma(A^{} z^{} + b^{}) \in$ ℝ'.format(i, i-1, i-1, i-1, i-1), 
                               fontdict = {'fontsize' : 18})
                 else:
@@ -260,7 +285,7 @@ def simulate(samples, features=2, data_="blobs", architecture=[2, 2, 1]):
 if __name__ == "__main__":
     #simulate(8, data_='blobs')
     #simulate(25, features=1, data_='blobs', architecture=[1, 2, 1])
-    simulate(8, features=1, data_='q_random', architecture=[1, 2, 1])
+    simulate(42, features=1, data_='q_random', architecture=[1, 3, 1])
     
     
 #    # Just plot the activation functions 
