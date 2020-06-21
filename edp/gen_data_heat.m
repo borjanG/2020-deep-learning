8a87380f54de51d89100b389d722dc45eb6653c8
borjanG <geshkovskiborjan@gmail.com>
2020-06-21 20:39:03 +0200

    added cubic heat data driven

---
+++ edp/gen_data_heat.m
@@ -0,0 +1,26 @@
+%% Burgers equation and chaos
+nn = 256;
+steps = 200;
+
+dom = [-8 8]; x = chebfun('x',dom); tspan = linspace(0,10,steps+1);
+S = spinop(dom, tspan);
+S.lin = @(u) + 0.1*diff(u,2);
+%S.nonlin = @(u) - 0.5*diff(u.^2); % spin cannot parse "u.*diff(u)"
+S.nonlin = @(u) -0.5*u.^3;
+%S.nonlin = @(u) -0.5*u;
+S.init = -sin(pi*x/8);
+%S.init = cos(pi*x/8);
+u = spin(S,nn,1e-4, 'plot', 'off');
+
+usol = zeros(nn,steps+1);
+for i = 1:steps+1
+    usol(:,i) = u{i}.values;
+end
+
+x = linspace(-8,8,nn+1);
+x = x(1:end-1);
+t = tspan;
+% pcolor(t,x,real(usol)); shading interp, axis tight, colormap(jet);
+save('burgers_sine.mat','t','x','usol')
+
+
